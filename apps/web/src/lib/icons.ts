03bfb6f1f3f8e57a6081764bfea94a12e9895a8a
RAGHub <sachncs@gmail.com>
2026-08-31 08:36:05 +0530

    feat(ui): lucide icon re-exports for consistent naming

---
+++ apps/web/src/lib/icons.ts
@@ -0,0 +1,77 @@
+import {
+  ArrowRight as _ArrowRight,
+  ArrowUpRight as _ArrowUpRight,
+  ArrowUp as _ArrowUp,
+  ArrowLeft as _ArrowLeft,
+  Check as _Check,
+  ChevronRight as _ChevronRight,
+  CircleAlert as _CircleAlert,
+  CircleCheck as _CircleCheck,
+  CircleDot as _CircleDot,
+  CircleStop as _CircleStop,
+  Clock as _Clock,
+  Eye as _Eye,
+  EyeClosed as _EyeClosed,
+  EyeOff as _EyeOff,
+  File as _File,
+  FilePlus as _FilePlus,
+  FileText as _FileText,
+  Folder as _Folder,
+  Key as _Key,
+  Loader as _Loader,
+  LogOut as _LogOut,
+  MessageSquare as _MessageSquare,
+  Moon as _Moon,
+  PanelLeft as _PanelLeft,
+  Plus as _Plus,
+  Search as _Search,
+  Send as _Send,
+  Settings as _Settings,
+  Shield as _Shield,
+  Sparkles as _Sparkles,
+  SunDim as _SunDim,
+  Trash as _Trash,
+  Upload as _Upload,
+  User as _User,
+  Users as _Users,
+  X as _X,
+  Zap as _Zap,
+} from "lucide-react";
+
+export const ArrowRight = _ArrowRight;
+export const ArrowUpRight = _ArrowUpRight;
+export const ArrowUp = _ArrowUp;
+export const ArrowLeft = _ArrowLeft;
+export const Check = _Check;
+export const ChevronRight = _ChevronRight;
+export const Warning = _CircleAlert;
+export const CheckCircle = _CircleCheck;
+export const CircleDot = _CircleDot;
+export const Stop = _CircleStop;
+export const Clock = _Clock;
+export const Eye = _Eye;
+export const EyeClosed = _EyeClosed;
+export const EyeOff = _EyeOff;
+export const File = _File;
+export const FilePlus = _FilePlus;
+export const FileText = _FileText;
+export const Folder = _Folder;
+export const Key = _Key;
+export const CircleNotch = _Loader;
+export const LogOut = _LogOut;
+export const ChatCircle = _MessageSquare;
+export const Moon = _Moon;
+export const PanelLeft = _PanelLeft;
+export const Plus = _Plus;
+export const MagnifyingGlass = _Search;
+export const Send = _Send;
+export const Gear = _Settings;
+export const Shield = _Shield;
+export const Sparkles = _Sparkles;
+export const SunDim = _SunDim;
+export const Trash = _Trash;
+export const Upload = _Upload;
+export const User = _User;
+export const Users = _Users;
+export const X = _X;
+export const Lightning = _Zap;
